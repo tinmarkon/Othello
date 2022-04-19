4f9a93d8ce4a5031d114333bae15bf4bf01cd5b5
Tin Markon <tin.markon@stroka.si>
2022-04-19 11:54:59 +0200

    Dodan razred inteligenca

---
+++ Othello/src/inteligenca/Inteligenca.java
@@ -0,0 +1,17 @@
+package inteligenca;
+
+import splosno.KdoIgra;
+
+public class Inteligenca extends splosno.KdoIgra {
+	
+	public Inteligenca() {
+		KdoIgra team = new KdoIgra("FIZFIN");
+		// TODO konstruktor za razred kdo igra?
+	}
+	
+	public Poteza izberiPotezo(Igra igra) {
+		// TODO implementacija inteligentnega igralca igre Othello
+		// MAKS 5sec
+	}
+
+}
