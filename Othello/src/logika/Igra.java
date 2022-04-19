120e8a196c4a796b1f73f1d4e7b55b47cc98a9bc
Tin Markon <tin.markon@stroka.si>
2022-04-19 11:54:52 +0200

    Dodan class Igra

---
+++ Othello/src/logika/Igra.java
@@ -0,0 +1,20 @@
+package logika;
+
+import splosno.Poteza;
+
+public class Igra {
+	
+	public Igra() {
+		
+	// TODO ustvari mrežo 8x8, s kakšnim tipom jo predstaviti?
+	// TODO nastavi zaèetno postavitev in da zaène èrni
+	}
+	
+	
+	public boolean odigraj(Poteza poteza) {
+		// TODO metoda vraèa true/false èe je poteza veljavna
+		// Poteza(0,0) je èisto levo zgoraj in Poteza(7,7) je èisto desno spodaj
+		return false;
+	}
+
+}
