88716af5ef3ade758d83ec1720db759dc61e805f
lodrantk <katarina.lodrant@gmail.com>
2022-05-11 00:34:38 +0200

    Spremenila class po zgledu polja iz TicTacToe. Polje ima le barvo, ne pa koordinat. Dodala funkcijo polje.nasprotno(), ki vrne nasprotno vrednost barve.

--- Othello/src/logika/Polje.java
+++ Othello/src/logika/Polje.java
@@ -1,43 +1,13 @@
-package logika;
+packpackage logika;
+public enum Polje {
+	BLACK, WHITE, PRAZNO
 
-public class Polje {
-	// na Deski je 8 x 8 Polj, ki imajo lahko VREDNOST: ČRNO, BELO ali PRAZNO
-	// vsako polje ima svoji KOORDINATI x in y
-	
-	public static enum Vrednost {
-		BLACK, WHITE, PRAZNO;
-	}
-	
-	private final int x;
-	private final int y;
-	private Vrednost vrednost;
-	
-	public Polje(int x, int y) {
-		this.x = x;
-		this.y = y;
-		this.vrednost = Vrednost.PRAZNO;
+	public Polje nasprotno() { //deluje samo, če je polje belo ali crno!!!
+		if (this == PRAZNO) {
+			System.out.println("Tega polja pa ne znam obrnit.");
+			return PRAZNO;
+		}
+		else return (this == BLACK ? WHITE : BLACK);
 	}
 
-	public Vrednost getVrednost() {
-		return vrednost;
 	}
-
-	public void setVrednost(Vrednost vrednost) {
-		this.vrednost = vrednost;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-	
-	public Vrednost obrniVrednost() {
-    // vrne obrnjeno vrednost: črno spremeni v belo in obratno.
-	// morda bolje ce kar obrne this.vrednost = novoVrednost in nic ne vrne?;
-	return (this.vrednost == Vrednost.BLACK ? Vrednost.WHITE : Vrednost.BLACK);
-    // se izjema, ce je vrednost prazno in se ga ne da obrnit
-	}
-}
