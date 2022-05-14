b1c7412d5dcb610e6daaa1ae84607253b5df3ce2
Tin Markon <tin.markon@stroka.si>
2022-05-14 19:35:31 +0200

    Hiter push na server ker mi bo zmanjkalo baterije :(

---
+++ Othello/src/vodja/Vodja.java
@@ -0,0 +1,45 @@
+package vodja;
+
+import java.util.Map;
+
+import logika.Igra;
+import logika.Igralec;
+import splosno.KdoIgra;
+
+public class Vodja {
+	
+	public static Map<Igralec,VrstaIgralca> vrstaIgralca;
+	public static Map<Igralec,KdoIgra> kdoIgra;
+	
+	public static Igra igra = null;
+	
+	public static boolean clovekNaVrsti = false;
+		
+	public static void igramoNovoIgro () {
+		igra = new Igra ();
+		igramo ();
+	}
+	
+	public static void igramo () {
+		okno.osveziGUI();
+		switch (igra.stanje()) {
+		case ZMAGA_O: 
+		case ZMAGA_X: 
+		case NEODLOCENO: 
+			return; // odhajamo iz metode igramo
+		case V_TEKU: 
+			Igralec igralec = igra.naPotezi();
+			VrstaIgralca vrstaNaPotezi = vrstaIgralca.get(igralec);
+			switch (vrstaNaPotezi) {
+			case C: 
+				clovekNaVrsti = true;
+				break;
+			case R:
+				igrajRacunalnikovoPotezo ();
+				break;
+			}
+		}
+	}
+	
+	
+}
