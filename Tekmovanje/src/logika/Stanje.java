362a8d32dd8981a8b88771acdbc988976eb13045
lodrantk <katarina.lodrant@gmail.com>
2022-05-27 00:27:53 +0200

    Dodala toString() za izpis.

--- Tekmovanje/src/logika/Stanje.java
+++ Tekmovanje/src/logika/Stanje.java
@@ -3,3 +3,11 @@ package logika;
 public enum Stanje {
 	V_TEKU, ZMAGA_B, ZMAGA_W, NEODLOCENO;
 }
+
+public String toString() {
+	if (this == V_TEKU) return "V TEKU";
+	else if (this == ZMAGA_B) return "ZMAGA B";
+	else if (this == ZMAGA_W) return "ZMAGA W";
+	else return "NEODLOCENO";
+	}
+}
