c85db80a8254be852e56a5f4201276bf28b93ec1
Tin Markon <tin.markon@stroka.si>
2022-05-26 02:04:56 +0200

    Popravil funkcijo ocenjevanja da ocenjuje razmerje potez med igralcema

--- Othello/src/inteligenca/OceniPozicijo.java
+++ Othello/src/inteligenca/OceniPozicijo.java
@@ -6,9 +6,7 @@ import logika.Igralec;
 public class OceniPozicijo {
 
 	public static int oceniPozicijo(Igra igra, Igralec jaz) {
-		int[] zetoni = igra.prestejZetone();
-		int ocena = (jaz == Igralec.BLACK ? zetoni[0] - zetoni[1] : -(zetoni[0] - zetoni[1]));
-		return ocena;
+			return igra.poteze().size() - igra.nasprotnikovePoteze().size();
 	}
 
 }
