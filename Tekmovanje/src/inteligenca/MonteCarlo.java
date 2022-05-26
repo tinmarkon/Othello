77b3cec54ea1477e4021eb6613a09308380bbabb
lodrantk <katarina.lodrant@gmail.com>
2022-05-27 00:25:13 +0200

    Algoritem Monte Carlo Tree Search za inteligentno igranje Othella.

--- Tekmovanje/src/inteligenca/MonteCarlo.java
+++ Tekmovanje/src/inteligenca/MonteCarlo.java
@@ -1,41 +1,142 @@
 package inteligenca;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 import logika.Igra;
+import logika.Igralec;
 import logika.Stanje;
 import splosno.Poteza;
 
-public class MonteCarlo extends Inteligenca {
-	
-	/* Določiš št. iteracij. Ponavljaš toliko iteracij. Izbereš najboljšo možnost. */
-	
+public class MonteCarlo extends Inteligenca{
+
+	private static final int ZMAGA = 1;
+	private static final double NEODLOCENO = 0.5;
+	private static final int MAX_CAS = 4900; //ms
+
 	@Override
-	public Poteza izberiPotezo (Igra igra) {
-		// Ta metoda izbere najboljso potezo glede na simulacijo
+	public Poteza izberiPotezo(Igra igra) {
+        /* Iščemo najboljšo izmed možnih potez z uporabo algoritma MONTE CARLO TREE SEARCH.
+        Trajanje iskanja omejimo na 4,8 s. */
+
+		//System.out.print(igra.naPotezi() + " Izbiram MCTS potezo. ");
+
+		long start = System.currentTimeMillis();
+		long end = start + MAX_CAS;
+
+		Igralec igralec = igra.naPotezi();
+
+		if (igra.poteze().size() == 1) {
+			//System.out.println("Igral sem edino možno potezo.");
+			return igra.poteze().get(0);
+		}
+
+		/* Drevo začneš z deblom = začetno pozicijo igre. */
+		Drevo drevo = new Drevo();
+		Vozlisce deblo = drevo.getDeblo();
+		deblo.getStatus().setGamePosition(igra);
+
+		while (System.currentTimeMillis() < end) {
+			// (1) SELEKCIJA: premaknemo se v najbolj obetaven list na trenutnem drevesu
+			Vozlisce obetavnoVozlisce = izberiObetavnoVozlisce(deblo);
+
+            /* (2) RAZVOJ: ustvarimo vozlišča otrok obetavnoVozlisce in jih shranimo v njegov slovarOtrok.
+            To storimo le v primeru, ko obetavnoVozlišče še ni končno (ima otroke). */
+			if (obetavnoVozlisce.getStatus().getGamePosition().stanje() == Stanje.V_TEKU)
+				razvijVozlisce(obetavnoVozlisce);
+
+			// (3) SIMULACIJA
+            /* Simulacijo vedno začnemo v vozlišču, ki ga še nikoli nismo obiskali.
+            Od tam naprej odigramo naključno igro in si zapolnimo rezultat. */
+			Vozlisce zaPreiskat =  ((obetavnoVozlisce.getSlovarOtrok().size() > 0) ? obetavnoVozlisce.nakljucenOtrok() : obetavnoVozlisce);
+			Stanje rezultatSimulacije = simulirajNakljucnoIgro(zaPreiskat, igralec);
+
+			// (4) POSODBI OCENE glede na rezultatSimulacije
+			propagirajNazaj(zaPreiskat, rezultatSimulacije);
+		}
+		Vozlisce zmagovalnoVozlisce = deblo.najboljObiskanOtrok();
+		drevo.setDeblo(zmagovalnoVozlisce);
+
+		//System.out.println("Število obiskov zmagovalca: " + zmagovalnoVozlisce.getStatus().getObiski() + ".");
+		//System.out.println("Število zmag v zmagovalcu: " + zmagovalnoVozlisce.getStars().getStatus().getZmage() + ".");
+
+		return zmagovalnoVozlisce.getStars().getSlovarOtrok().get(zmagovalnoVozlisce); //vrnes potezo, ki te pripelje v zmagovalnoVozlisce
 	}
-	
-	public igrajDoKonca (Vozlisce vozlisce) {
-		/* Izbereš naključnega otroka in igraš igro. To ponavljaš dokler se igra ne zaključi. 
-		 * Glede na končno stanje posodobiš oceno vsem igranim otrokom = vsem staršem.
-		 * Vsem otrokom posodobiš obiske.
-		 */
-		
+
+	private Vozlisce izberiObetavnoVozlisce(Vozlisce deblo) {
+        /* Zacne v deblu in se premakne do konca veje (lista).
+        V vsakem koraku (vozliscu) se premakne v že ustvarjenega otroka z najboljso UCToceno. */
+
+		Vozlisce vozlisce = deblo;
+		while (vozlisce.getSlovarOtrok().size() != 0) {
+			vozlisce = najboljeOcenjenoVozlisce(vozlisce);
+		}
+		return vozlisce;
 	}
-	
-	public Poteza nakljucnaPoteza(ArrayList<Poteza> poteze) {
-		int index = (int)(Math.random() * poteze.size());
-		return poteze.get(index);
+
+	static Vozlisce najboljeOcenjenoVozlisce(Vozlisce vozlisce) {
+        /* Izmed vseh otrok danega vozlisca izbere tistega z najboljso (a) UCT oceno ali (b) skupnoOceno.
+		Slednja upošteva UCT oceno, v začetku igre preferira poteze v središču plošče, v
+		začetku igre teži k manj žetonom barve inteligenčnega igralca,
+		boljšo oceno dajo poteze v kotih desko, slabšo pa poteze, ki igralcu "podarijo" kote.
+		Ocenjevanje delno prirejeno po zgledu: https://royhung.com/reversi. */
+
+		List<Vozlisce> otroci = new ArrayList<>(vozlisce.getSlovarOtrok().keySet());
+		return Collections.max(otroci, Comparator.comparing(c -> c.UCTocena())); //Collections.max(otroci, Comparator.comparing(c -> c.skupnaOcena("max")));
 	}
-	
-	public void odigrajNakljucnoPotezo(Vozlisce vozlisce) {
-		/* Odigraš naključno potezo. V otrokovem vozlišču posodobiš obisk.*/
-		
+
+	public void razvijVozlisce(Vozlisce stars) {
+        /* Ustvari otroke (objekt tipa Vozlisce) za vhodnega starsa.
+        Nastavi starsa in jim pripise zmage = 0 in obiski = 0.
+        Otroke pospravi v staršev slovarOtrok. */
+		Map<Status, Poteza> moznaStanja = stars.getStatus().getMozniStatusi();
+		for (Status s: moznaStanja.keySet()) {
+			Vozlisce v = new Vozlisce(s);
+			v.setStars(stars);
+			stars.getSlovarOtrok().put(v, moznaStanja.get(s));
+		};
 	}
-	
-	public void posodobiOceno(Vozlisce vozlisce) {
-		/* Back propagation. Staršu posodabljaš oceno. */
-		
+
+	private void propagirajNazaj(Vozlisce v, Stanje koncnoStanje) {
+    	/* Začneš v vozlišču v, kjer si začel naključno igro, in greš nazaj do debla po vseh starših.
+        Vsakemu glede na izid igre po koncu naključne igre (koncnoStanje) posodobiš vozlisce.zmage ter povečaš vozlisce.obiski za 1.
+        Će naključno igro zmaga igralec, ki je na vrsti v danesm vozliscu, zmagam pristejemo 1, na neodloceno igro pa 0.5.
+        */
+
+		Vozlisce vozlisce = v;
+
+		while (vozlisce != null) { //dokler ne pridemo do debla
+			vozlisce.getStatus().povecajObiske();
+			if (koncnoStanje == Stanje.NEODLOCENO) vozlisce.getStatus().posodobiZmage(NEODLOCENO);
+
+			if (koncnoStanje == vozlisce.getStatus().getGamePosition().naPotezi().nasprotnik().getStanje()) vozlisce.getStatus().posodobiZmage(ZMAGA);
+			vozlisce = vozlisce.getStars();
+		}
 	}
-	
+	private Stanje simulirajNakljucnoIgro(Vozlisce vozlisce, Igralec igralec) {
+    	/* Igra delno-nakljucno igro iz vozlisca in vrne izid igre v zadnjem vozliscu - listu: zmaga_B, zmaga_W oz. NEODLOCENO.
+		Delno-naključno pomeni, da se izogiba zelo slabim pozicijam (takim, ki nasprotniku podarijo kote.)
+        Če smo želeli simulirati naključno igro iz lista v katerem zmaga nasprotnik, potem nastavimo zmage
+        njegovega starša na Integer.MIN_VALUE --> to ni dobra poteza.*/
+
+		Vozlisce kopija = new Vozlisce(vozlisce);
+		Status stanjeVozlisca = kopija.getStatus();
+		Stanje stanjeIgre = stanjeVozlisca.getGamePosition().stanje();
+
+		if (stanjeIgre == igralec.nasprotnik().getStanje()) {//vozlisce.getStatus().getGamePosition().naPotezi().getStanje()) {
+			kopija.getStars().getStatus().setZmage(Integer.MIN_VALUE);
+			return stanjeIgre;
+		}
+
+		while (stanjeIgre == Stanje.V_TEKU) {
+			/* Igraj naključne poteze. */
+			stanjeVozlisca.igrajNakljucno(); //igrajDelnoNakljucno();
+			stanjeIgre = stanjeVozlisca.getGamePosition().stanje();
+		}
+		return stanjeIgre;
+
+	}
 }
--- Tekmovanje/src/inteligenca/Vozlisce.java
+++ Tekmovanje/src/inteligenca/Vozlisce.java
@@ -1,52 +1,162 @@
 package inteligenca;
 
+import java.util.List;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
-import logika.Igra;
-
+import logika.Igralec;
 import splosno.Poteza;
 
 public class Vozlisce {
-	public final Igra gamePosition;
-	private int obiski;
-	// Število zmag če iz tega nodea igramo random poteze
-	private double ocena;
-	private Vozlisce stars;
-	private Map<Poteza, Vozlisce> otroci = new HashMap<Poteza, Vozlisce>();
-	
-	public Vozlisce(Igra gamePosition, Vozlisce stars) {
-		this.gamePosition = gamePosition;
-		this.obiski = 1; //ali 0
-		this.ocena = 0;
+    /* Objekt vozlisce predstavlja razvejišče (angl. Node) v drevesu.
+    Vozlišče ima
+    (1) stanje -- predstavlja ga class Status (da ne mešamo z razredom logika.Stanje,
+        ki predstavlja stanje igre) in ima 3 polja:
+        * Igra gamePosition: Stanje na igralni deski v tem vozlišču je unikatno posameznemu vozlišču;
+        * int obiski: kolikokrat smo že izbrali to vozlišče in v naslendjih korakih simulirali naključno
+        igro;
+        * int zmage: kolikokrat smo zmagali, ko smo izbrali to vozlišče in naprej do konca igrali naključno
+        igro (+1 za zmago, +0,5 za nedoločen rezultat oz. odvisno od tega kdo je na vrsti);
+    (2) starša (tipa vozlišče) in
+    (3) slovarOtrok (ključi so vozlišča orok, vrednosti pa poteze, ki jih moramo igrati
+        na gamePosition, da pridemo v to vozlišče). */
+
+	final double c = 1/Math.sqrt(2);
+	private static final Poteza[] zeloSlabePoteze = {new Poteza(1, 1), new Poteza(6, 1), new Poteza(1, 6), new Poteza(6, 6)};
+	private static final Poteza[] slabePoteze = {new Poteza(0, 1), new Poteza(1, 0), new Poteza(6, 0), new Poteza(0, 6), new Poteza(7, 1), new Poteza(1, 7), new Poteza(7, 6), new Poteza(6, 7)};
+	private static final Poteza[] dobrePoteze = {new Poteza(0, 0), new Poteza(7, 7), new Poteza(0, 7), new Poteza(7, 0)};
+
+	private Status status;
+	private  Vozlisce stars;
+	private Map<Vozlisce, Poteza> slovarOtrok;
+
+	public Vozlisce() {
+		this.status = new Status();
+		this.slovarOtrok = new HashMap<Vozlisce, Poteza>();
+	}
+
+	public Vozlisce(Status status) {
+		this.status = status;
+		this.slovarOtrok = new HashMap<Vozlisce, Poteza>();
+	}
+
+	public Vozlisce(Status status, Vozlisce stars, Map<Vozlisce, Poteza> otroci) {
+		this.stars = stars;
+		this.status = status;
+		this.slovarOtrok = otroci;
+	}
+
+	public Vozlisce(Vozlisce vozlisce) {
+		/* Kopija vozlisca. */
+		this.slovarOtrok = new HashMap<Vozlisce, Poteza>();
+		this.status = new Status(vozlisce.getStatus());
+
+		if (vozlisce.getStars() != null) //ce je to prvo vozlisce = deblo in nima starsa
+			this.stars = vozlisce.getStars();
+
+		List<Vozlisce> otroci = new ArrayList<Vozlisce>(slovarOtrok.keySet());
+		for (int i = 0; i < vozlisce.slovarOtrok.size(); i++) {
+			this.slovarOtrok.put(otroci.get(i), slovarOtrok.get(otroci.get(i))); //moram tu ustvarit novo vozlišče?
+			//this.slovarOtrok.put(new Vozlisce (otroci.get(i)), slovarOtrok.get(otroci.get(i)));
+		}
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Vozlisce getStars() {
+		return stars;
+	}
+
+	public void setStars(Vozlisce stars) {
 		this.stars = stars;
 	}
-	
-	public double getOcena() {
-		return ocena;
+
+	public Map<Vozlisce, Poteza> getSlovarOtrok() {
+		return slovarOtrok;
 	}
-	
-	public double getObiski() {
-		return obiski;
+
+	public Vozlisce nakljucenOtrok() {
+		Set<Vozlisce> otrociSet = slovarOtrok.keySet();
+		List<Vozlisce> otroci = new ArrayList<>(otrociSet);
+
+		Random r = new Random();
+		int index = r.nextInt(otroci.size());
+		return otroci.get(index);
 	}
-	
-	public Map<Poteza, Vozlisce> getOtroci() {
-		Map<Poteza, Vozlisce> otroci = new HashMap<Poteza, Vozlisce>();
-		for (Poteza p: gamePosition.poteze()) {
-			Igra kopijaIgre = new Igra(gamePosition);
-			kopijaIgre.odigraj(p);
-			otroci.put(p, new Vozlisce(kopijaIgre, this));
+
+	public Vozlisce najboljObiskanOtrok() {
+		List<Vozlisce> otroci = new ArrayList<>(this.getSlovarOtrok().keySet());
+		Vozlisce najboljsi = Collections.max(otroci, Comparator.comparing((Vozlisce c) -> c.getStatus().getObiski()));
+		return najboljsi;
+	}
+
+	public void printVozlisce(){
+		this.getStatus().getGamePosition().printIgra();
+		System.out.println("Zmage: " + this.getStatus().getZmage());
+		System.out.println("Obiski: " + this.getStatus().getObiski());
+	}
+
+	public double UCTocena() {
+		/* UCT (Upper Confidence Bound) ocena vozlišča za selekcijo. */
+		int N = this.stars.getStatus().getObiski();
+		int n = this.getStatus().getObiski();
+		double zmage = this.getStatus().getZmage();
+		if (n == 0) {
+			/* Radi bi obiskali vse otroke. */
+			return Integer.MAX_VALUE;
 		}
-		return otroci;	
-	}
-	
-	public void printVozlisce() {
-		gamePosition.printIgra();
-	}
-	
-	public void getPoteze() {
-		gamePosition.poteze();
+		double o = zmage / n + c * Math.sqrt(Math.log(N)/n);
+		return o;
+
 	}
 
+	// ----- za potencialno izboljšavo MCTS algoritma -----
+	public double notranjePoteze() {
+		/* V začetku igre naj preferira poteze v notranjosti deske */
+		int i = this.getStars().getSlovarOtrok().get(this).getX();
+		int j = this.getStars().getSlovarOtrok().get(this).getY();
+		return 1 / Math.sqrt( Math.pow(i - 3.5, 2) + Math.pow(j - 3.5, 2));
+	}
 
+	public double kaznujPozresnost() {
+		/* V začetku igre naj ne želi imeti več žetonov svoje barve. */
+		int[] st = this.getStatus().getGamePosition().prestejZetone();
+		int crni = st[0];
+		int beli = st[1];
+		int t = 1;
+		if (this.getStatus().getGamePosition().naPotezi() == Igralec.WHITE) t = -1;
+		return t * (beli - crni) /  Math.pow(beli + crni, 4);
+	}
+
+	public double ocenaPoteze() {
+		/* Dobre so poteze v kotih deske, slabe so poteze, ki dajo kote nasprotniku.*/
+		Poteza poteza = this.getStars().getSlovarOtrok().get(this);
+		double ocenaPoteze = 0;
+		if (Arrays.asList(slabePoteze).contains(poteza)) ocenaPoteze = -0.55;
+		else if (Arrays.asList(zeloSlabePoteze).contains(poteza)) ocenaPoteze = -100;
+		else if (Arrays.asList(dobrePoteze).contains(poteza)) ocenaPoteze = 1.5;
+		return ocenaPoteze;
+	}
+
+	public double skupnaOcena(String minmax) {
+		int[] st = this.getStatus().getGamePosition().prestejZetone();
+		if ((st[0] + st[1]) > 50) return this.UCTocena();
+
+		else {
+			if (minmax == "min") return this.UCTocena() * (1 - this.notranjePoteze() - this.ocenaPoteze() + this.kaznujPozresnost());
+			else return this.UCTocena() * (1 + this.notranjePoteze() + this.ocenaPoteze() - this.kaznujPozresnost());
+		}
+	}
 }
