d2b867658858c35c20fae35d16643a0d51f6ffbd
lodrantk <katarina.lodrant@gmail.com>
2022-06-15 12:50:15 +0200

    Veliki napisi na gumbih.

--- src/main/java/si/lodrant/othello/gui/Strings.java
+++ src/main/java/si/lodrant/othello/gui/Strings.java
@@ -6,11 +6,11 @@ public class Strings {
     public final static String IGRALEC_R2 = "Pameten nasprotnik";
     public final static String IGRALEC_R3 = "Genialen nasprotnik";
     public final static String START_GUMB = "Začni novo igro";
-    public final static String EN_IGRALEC = "En igralec";
-    public final static String DVA_IGRALCA = "Dva igralca";
-    public final static String NAVODILA = "Navodila";
+    public final static String EN_IGRALEC = "EN IGRALEC";
+    public final static String DVA_IGRALCA = "DVA IGRALCA";
+    public final static String NAVODILA = "NAVODILA";
     public final static String NAMIG_GUMB = "Namig";
-    public final static String IZHOD = "Izhod";
+    public final static String IZHOD = "IZHOD";
     public final static String POTEZE_GUMB = "Možne poteze";
     public final static String RAZVELJAVI_GUMB = "Razveljavi potezo!";
     public final static String START_STATUS = "Izberite igralca in začnite igro!";
